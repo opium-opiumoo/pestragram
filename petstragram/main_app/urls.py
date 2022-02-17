d8a653f46a2ca460d99dac0a510685ca40835c6f
slavi <slavi6pm@gmail.com>
2022-02-17 18:22:58 +0200

    Adding some of the form views

--- petstragram/main_app/urls.py
+++ petstragram/main_app/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from petstragram.main_app.views import like_pet, show_home, show_dashboard, show_profile, show_pet_photo_details
+from petstragram.main_app.views import create_pet, create_pet_photo, create_profile, delete_pet, delete_profile, edit_pet, edit_pet_photo, edit_profile, like_pet, show_home, show_dashboard, show_profile, show_pet_photo_details
 
 urlpatterns = (
     path('', show_home, name='index'),
     path('dashboard/', show_dashboard, name='dashboard'),
+
     path('profile/', show_profile, name='profile'),
+    path('profile/create/', create_profile, name='create profile'),
+    path('profile/edit/', edit_profile, name='edit profile'),
+    path('profile/delete/', delete_profile, name='delete profile'),
+
     path('photo/details/<int:pk>/', show_pet_photo_details, name='pet photo details'),
     path('photo/like/<int:pk>/', like_pet ,name='like pet photo'),
+    path('photo/add/', create_pet_photo ,name='add pet photo'),
+    path('photo/edit/<int:pk>/', edit_pet_photo ,name='edit pet photo'),
+    
+    path('pet/create', create_pet, name='create pet'),
+    path('pet/edit/<int:pk>/', edit_pet, name='edit pet'),
+    path('pet/delete/<int:pk>/', delete_pet, name='delete pet'),
 )
--- petstragram/main_app/views.py
+++ petstragram/main_app/views.py
@@ -1,5 +1,6 @@
 from webbrowser import get
 from django.shortcuts import redirect, render
+from petstragram.main_app.forms import ProfileForm
 
 from petstragram.main_app.models import PetPhoto, Profile
 
@@ -19,6 +20,9 @@ def show_home(request):
 
 def show_dashboard(request):
     profile = get_profile()
+    if not profile:
+        return redirect('401_error.html')
+
     pet_photos = set(PetPhoto.objects\
         .filter(tagged_pets__user_profile=profile))
     context = {
@@ -28,7 +32,16 @@ def show_dashboard(request):
     return render(request, 'dashboard.html', context)
 
 def show_profile(request):
-    return render(request, 'profile_details.html')
+    profile = get_profile()
+    total_likes_count = sum(pp.likes for pp in PetPhoto.objects.filter(tagged_pets__user_profile=profile))
+    total_images_count = len(PetPhoto.objects.filter(tagged_pets__user_profile=profile).distinct())
+    context = {
+        "profile": profile,
+        "total_likes_count": total_likes_count,
+        "total_images_count": total_images_count,
+    }
+
+    return render(request, 'profile_details.html', context)
 
 def show_pet_photo_details(request, pk):
     pet_photo = PetPhoto.objects \
@@ -47,3 +60,41 @@ def like_pet(request, pk):
     pet_photo.save()
 
     return redirect('pet photo details', pk)
+
+def create_profile(request):
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ProfileForm()
+
+    context = {
+        'form': form,
+    }
+
+
+    return render(request, 'profile_create.html', context)
+
+def edit_profile(request):
+    return render(request, 'profile_edit.html')
+
+def delete_profile(request):
+    return render(request, 'profile_delete.html')
+
+def create_pet(request):
+    return render(request, 'pet_create.html')
+
+def edit_pet(request):
+    return render(request, 'pet_edit.html')
+
+def delete_pet(request):
+    return render(request, 'pet_delete.html')
+
+def create_pet_photo(request):
+    return render(request, 'photo_create.html')
+
+def edit_pet_photo(request):
+    return render(request, 'photo_edit.html')
